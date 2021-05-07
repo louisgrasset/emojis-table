079bc6e4cd3f5a3787ed8f3417de6b10604e05c2
Louis Grasset <louisgrasset@protonmail.com>
2021-05-07 16:51:21 +0200

    chore: code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,37 @@
 import './App.scss';
 
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState, useRef } from 'react';
 import { browser } from "webextension-polyfill-ts";
-import { AutoSizer, Grid } from 'react-virtualized';
 
-import Search from './Search';
+import { History } from './components/History';
+import { List } from './components/List';
+import { Search } from './components/Search';
+import { Attribution } from './components/Attribution';
 
-const emojis = require("emojilib");
+const rawEmojis = require("emojilib");
 
 function App() {
-
+  const elementRef = useRef<HTMLInputElement>(null);
   const [search, setSearch] = useState('');
-  let columnCount = 5;
+  const [columnCount, setColumnCount] = useState(5);
+  const [emojisHistory, setEmojisHistory] = useState<{ emojis: string[] }>({ emojis: ['🍋', '🍣', '🌱', '🌸', '🔥'] });
 
-  const emojisData = useMemo(
-    () => Object.keys(emojis)
-      .map((e: any) => (({ character: e, keywords: emojis[e].map((k: string) => k.replace(/[_-]/g, " ")) }))),
+  const emojis = useMemo(
+    () => Object.keys(rawEmojis)
+      .map((e: any) => (({ character: e, keywords: rawEmojis[e].map((k: string) => k.replace(/[_-]/g, " ")) }))),
     []);
 
-  let placeholder = useMemo(() => emojisData[Math.floor(Math.random() * emojisData.length)].keywords[0], [emojisData]);
+  const placeholder = useMemo(() => emojis[Math.floor(Math.random() * emojis.length)].keywords[0], [emojis]);
 
-  const emojisList = useMemo(
-    () => emojisData.filter(e => search.length > 0 ? (e.keywords.filter((k: string) => k.includes(search)).length ? e : null) : e),
-    [search, emojisData]);
+  const emojisFiltered = useMemo(
+    () => emojis.filter(e => search.length > 0 ? (e.keywords.filter((k: string) => k.includes(search)).length ? e : null) : e),
+    [search, emojis]);
 
-  const emojisCharacters = useMemo(() => emojisList.map(e => e.character), [emojisList]);
+  const emojisCharacters = useMemo(() => emojisFiltered.map(e => e.character), [emojisFiltered]);
+
   let rowCount = useMemo(() => Math.ceil(emojisCharacters.length / columnCount), [emojisCharacters, columnCount])
-  const [emojisHistory, setEmojisHistory] = useState<{ emojis: string[] }>({ emojis: [] });
 
-  useEffect(() => {
-    browser.storage.local.get('history').then((data: any) => {
-      setEmojisHistory({ emojis: (data?.history ? data.history : ['🍋', '🍣', '🌱', '🌸', '🔥']) });
-    })
-  }, [])
-
-  useEffect(() => {
-    browser.storage.local.set({ 'history': emojisHistory.emojis });
-  }, [emojisHistory])
+  const handleColumns = useCallback(() => { if (elementRef.current) { setColumnCount(Math.ceil(elementRef?.current.getBoundingClientRect().width / 60)) } }, [elementRef])
 
   const saveEmoji = useCallback((e: string) => {
     let history: string[] = emojisHistory.emojis ? emojisHistory.emojis : [];
@@ -49,56 +44,29 @@ function App() {
     }
   }, [emojisHistory, columnCount]);
 
-  const copyEmoji = useCallback((e: string) => {
-    const clipboard = navigator.clipboard;
-    clipboard.writeText(e).then(() => saveEmoji(e))
-  }, [saveEmoji]);
+  const copyEmoji = useCallback((e: string) => navigator.clipboard.writeText(e).then(() => saveEmoji(e)), [saveEmoji]);
+
+
+  useEffect(() => {
+    browser.storage.local.get('history').then((data: any) => {
+      setEmojisHistory({ emojis: (data?.history ? data.history : ['🍋', '🍣', '🌱', '🌸', '🔥']) });
+    })
+  }, [])
+
+  useEffect(() => {
+    browser.storage.local.set({ 'history': emojisHistory.emojis });
+  }, [emojisHistory])
+
+  useEffect(() => {
+    window.addEventListener('resize', handleColumns);
+    return () => window.removeEventListener('resize', handleColumns);
+  }, [handleColumns])
+
   return (
-    <div className="app" >
-      <Search search={search} setSearch={setSearch} placeholder={placeholder}></Search>
-      <div className="emojis-list--emojis-history" style={{ gridTemplateColumns: `repeat(${columnCount}, 1fr)` }}>
-        {
-          emojisHistory.emojis?.map((emoji, key) => (
-            <div className={'emojis-list__item'} key={key} onClick={() => copyEmoji(emoji)}>
-              {emoji}
-            </div>
-          ))
-        }
-      </div>
-      {
-        emojisCharacters.length === 0 && (
-          <div className="emojis-list--emojis-note">
-            <h1>😔</h1>
-            <p>Nothing...</p>
-          </div>
-        )
-      }
-      <div style={{ position: 'relative', height: 'calc(100% - 7rem)' }}>
-        {
-          <AutoSizer>
-            {({ width, height }) => (
-              <Grid
-                cellRenderer={({ columnIndex, key, rowIndex, style }) => {
-                  const emoji = emojisCharacters[rowIndex * columnCount + columnIndex];
-                  return <div className={'emojis-list__item'} key={key} style={style} onClick={() => copyEmoji(emoji)}>
-                    {emoji}
-                  </div>
-                }
-                }
-                className={'emojis-list'}
-                columnWidth={width / columnCount}
-                columnCount={columnCount}
-                height={height}
-                overscanColumnCount={0}
-                overscanRowCount={0}
-                rowHeight={50}
-                rowCount={rowCount}
-                width={width}
-              />
-            )}
-          </AutoSizer>
-        }
-      </div>
+    <div className="app" ref={elementRef}>
+      <Search search={search} setSearch={setSearch} placeholder={placeholder} />
+      <History history={emojisHistory} copyEmoji={copyEmoji} />
+      <List emojis={emojisCharacters} copyEmoji={copyEmoji} rowCount={rowCount} columnCount={columnCount} />
     </div>
   );
 }
