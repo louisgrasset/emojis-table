5195e618cb375903977a16d5233af57be0c0572f
Louis Grasset <louisgrasset@protonmail.com>
2021-05-07 15:11:14 +0200

    feat: add components

---
+++ src/components/Attribution/Attribution.tsx
@@ -0,0 +1,13 @@
+import './Attribution.scss';
+
+export function Attribution() {
+    return (
+        <div className="attribution">
+            <div className="attribution__container">
+                <a href="https://twitter.com/louisgrasset">Tw</a>&nbsp;·&nbsp;
+                <a href="https://github.com/louisgrasset">Gh</a>&nbsp;·&nbsp;
+                <span>Made by <a href="https://louisgrasset.fr">Louis&nbsp;Grasset</a></span>
+            </div>
+        </div>
+    );
+}
