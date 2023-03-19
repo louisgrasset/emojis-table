d2fa2646c498febcf1d4a116ba240381495e6a10
Louis Grasset <louisgrasset@protonmail.com>
2023-03-20 00:54:28 +0100

    chore: additional lint

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,21 +1,21 @@
-import {defineManifest} from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
-import packageJson from './package.json'
+import { defineManifest } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+import packageJson from "./package.json";
 
-const {version} = packageJson
+const { version } = packageJson;
 
 // Convert from Semver (example: 0.1.0-beta6)
 const [major, minor, patch] = version
-    // can only contain digits, dots, or dash
-    .replace(/[^\d.-]+/g, '')
-    // split into version parts
-    .split(/[.-]/)
+  // can only contain digits, dots, or dash
+  .replace(/[^\d.-]+/g, "")
+  // split into version parts
+  .split(/[.-]/);
 
-export default defineManifest(async (env) => {
-   return {
-        manifest_version: 3,
-        // up to four numbers separated by dots
-        version: `${major}.${minor}.${patch}`,
-       ...manifest,
-    };
-})
+export default defineManifest(async (_env) => {
+  return {
+    manifest_version: 3,
+    // up to four numbers separated by dots
+    version: `${major}.${minor}.${patch}`,
+    ...manifest,
+  };
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import {crx} from '@crxjs/vite-plugin'
-import manifest from './manifest.config'
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.config";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), crx({manifest}),
-    ],
-})
+  plugins: [react(), crx({ manifest })],
+});
