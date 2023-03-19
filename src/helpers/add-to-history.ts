be38aed4bfe514936fb546acbc1a4c487f117a3b
Louis Grasset <louisgrasset@protonmail.com>
2023-03-20 00:32:01 +0100

    fix: typo

--- src/helpers/add-to-history.ts
+++ src/helpers/add-to-history.ts
@@ -1,9 +1,9 @@
 export const addToHistory = (emojiToAdd: string) => {
   void chrome.storage.sync.get(["history"]).then((data) => {
     if (!data.history.includes(emojiToAdd)) {
-      const udpatedHistory = [...data.history.slice(1, 6), emojiToAdd];
+      const updatedHistory = [...data.history.slice(1, 6), emojiToAdd];
       void chrome.storage.sync
-        .set({ history: udpatedHistory })
+        .set({ history: updatedHistory })
         .catch((err) => console.error(err));
     }
   });
