7353a21b0b877c97a5988dec9e1c2c197e0467ee
Louis Grasset <louisgrasset@protonmail.com>
2025-04-01 23:12:58 +0200

    chore: manual lint; imports sorting

--- scripts/buildEmojiData.mjs
+++ scripts/buildEmojiData.mjs
@@ -1,45 +1,52 @@
-import emojis from "unicode-emoji-json" assert {type: "json"};
-import descriptions from "emojilib" assert {type: "json"};
+import { writeFileSync } from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import descriptions from "emojilib" with { type: "json" };
 import logUpdate from "log-update";
-import {writeFileSync} from 'fs';
-import path from 'path';
-import {fileURLToPath} from 'url';
+import emojis from "unicode-emoji-json" with { type: "json" };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const filePath = `${__dirname}/../src/data/`
+const filePath = `${__dirname}/../src/data/`;
 const size = Object.keys(emojis).length;
 
 const groups = new Set();
 const data = Object.entries(emojis).map(([emoji, data], index) => {
-    logUpdate(`🛠️Building emoji data [${index + 1}/${size}]...`);
-    groups.add(data.group);
+  logUpdate(`🛠️Building emoji data [${index + 1}/${size}]...`);
+  groups.add(data.group);
 
-    return {
-        ...data,
-        emoji: emoji,
-        description: descriptions[emoji] ? [data.name, ...descriptions[emoji]] : [data.name]
-    }
+  return {
+    ...data,
+    emoji: emoji,
+    description: descriptions[emoji]
+      ? [data.name, ...descriptions[emoji]]
+      : [data.name],
+  };
 });
 logUpdate(`✔️Build of ${size} emojis data finished!`);
 logUpdate.done();
 
 const dataByGroup = {};
-groups.forEach(group => {
-    let count = 0;
-    dataByGroup[group] = Object.values(data).filter(d => {
-        logUpdate(`🛠 Grouping emoji data for group "${group}" - ${d.emoji}`);
-        const result = d.group === group;
-        if (result) {
-            count++
-        }
-        return result
-    })
-    logUpdate(`✔️Group "${group}" finished [${count} emojis]!`);
-    logUpdate.done();
-})
+groups.forEach((group) => {
+  let count = 0;
+  dataByGroup[group] = Object.values(data).filter((d) => {
+    logUpdate(`🛠 Grouping emoji data for group "${group}" - ${d.emoji}`);
+    const result = d.group === group;
+    if (result) {
+      count++;
+    }
+    return result;
+  });
+  logUpdate(`✔️Group "${group}" finished [${count} emojis]!`);
+  logUpdate.done();
+});
 
 logUpdate(`✏️️️Writing database to file...`);
-writeFileSync(`${filePath}/emojidb-raw.json`, JSON.stringify(data), {encoding: 'utf-8'});
-writeFileSync(`${filePath}/emojidb.json`, JSON.stringify(dataByGroup), {encoding: 'utf-8'});
+writeFileSync(`${filePath}/emojidb-raw.json`, JSON.stringify(data), {
+  encoding: "utf-8",
+});
+writeFileSync(`${filePath}/emojidb.json`, JSON.stringify(dataByGroup), {
+  encoding: "utf-8",
+});
 logUpdate(`✔️Database written to file!`);
--- scripts/buildManifest.mjs
+++ scripts/buildManifest.mjs
@@ -1,63 +1,64 @@
+import { writeFileSync } from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import logUpdate from "log-update";
-import {writeFileSync} from 'fs';
-import path from 'path';
-import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const filePath = `${__dirname}/../`
+const filePath = `${__dirname}/../`;
 
-const BROWSERS = ['firefox', 'chromium'];
+const BROWSERS = ["firefox", "chromium"];
 
 if (!BROWSERS.includes(process.env?.BROWSER)) {
-    throw new Error(`Browser target not supported.`);
+  throw new Error(`Browser target not supported.`);
 }
 
 const browser = process.env.BROWSER;
 const commonManifest = {
-    "name": "Emojis table",
-    "description": "Get the right emojis from your browser in any tab!",
-    "action": {
-        "default_popup": "index.html"
-    },
-    "icons": {
-        "16": "icons/icon-16.png",
-        "32": "icons/icon-32.png",
-        "48": "icons/icon-48.png",
-        "96": "icons/icon-96.png",
-        "128": "icons/icon-128.png",
-        "256": "icons/icon-256.png"
-    },
-    "permissions": [
-        "storage"
-    ]
-}
+  name: "Emojis table",
+  description: "Get the right emojis from your browser in any tab!",
+  action: {
+    default_popup: "index.html",
+  },
+  icons: {
+    16: "icons/icon-16.png",
+    32: "icons/icon-32.png",
+    48: "icons/icon-48.png",
+    96: "icons/icon-96.png",
+    128: "icons/icon-128.png",
+    256: "icons/icon-256.png",
+  },
+  permissions: ["storage"],
+};
 
 const buildManifestFor = (browser) => {
-    logUpdate(`Building manifest for ${browser}`);
-    if(browser === 'firefox') {
-        logUpdate(`Manifest built for ${browser}`);
-        return {
-            ...commonManifest,
-            "browser_specific_settings": {
-                "gecko": {
-                    "id": "{3c793fa2-eef3-441a-b40e-fa77611b767a}",
-                    "strict_min_version": "109.0"
-                }
-            },
-        }
-    }
+  logUpdate(`Building manifest for ${browser}`);
+  if (browser === "firefox") {
+    logUpdate(`Manifest built for ${browser}`);
+    return {
+      ...commonManifest,
+      browser_specific_settings: {
+        gecko: {
+          id: "{3c793fa2-eef3-441a-b40e-fa77611b767a}",
+          strict_min_version: "109.0",
+        },
+      },
+    };
+  }
 
-    if(browser === 'chromium') {
-        logUpdate(`Manifest built for ${browser}`);
-        return {
-            ...commonManifest,
-        }
-    }
-}
+  if (browser === "chromium") {
+    logUpdate(`Manifest built for ${browser}`);
+    return {
+      ...commonManifest,
+    };
+  }
+};
 
 const manifest = buildManifestFor(browser);
 logUpdate.done();
 logUpdate(`Writing manifest to file`);
-writeFileSync(`${filePath}/manifest.json`, JSON.stringify(manifest), {encoding: 'utf-8'});
+writeFileSync(`${filePath}/manifest.json`, JSON.stringify(manifest), {
+  encoding: "utf-8",
+});
 logUpdate(`Manifest written to file`);
